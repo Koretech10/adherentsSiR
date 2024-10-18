5c4e98b7e77dd8fc1225a9bce56238d6503214d4
Aurélien OUDART <aoudart@hexanet.fr>
2024-10-18 13:36:05 +0200

    Cleanup root:manage command

--- src/Command/RootManageCommand.php
+++ src/Command/RootManageCommand.php
@@ -7,6 +7,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
@@ -31,15 +32,16 @@ class RootManageCommand extends Command
         // Conditions à respecter pour le mot de passe
         $output->writeln([
             "Vous êtes sur le point d'initialiser ou de modifier le mot de passe du compte root de l'application.",
-            "Ce mot de passe doit répondre aux exigences de sécurité suivantes :",
-            "    - 16 caractères minimum",
-            "    - Au moins une lettre minuscule",
-            "    - Au moins une lettre MAJUSCULE",
-            "    - Au moins un chiffre",
-            "    - Au moins un de ces caractères spéciaux : ! @ # $ % ^ & *"
+            'Ce mot de passe doit répondre aux exigences de sécurité suivantes :',
+            '    - 16 caractères minimum',
+            '    - Au moins une lettre minuscule',
+            '    - Au moins une lettre MAJUSCULE',
+            '    - Au moins un chiffre',
+            '    - Au moins un de ces caractères spéciaux : ! @ # $ % ^ & *',
         ]);
 
         // Pose la question
+        /** @var QuestionHelper $helper */
         $helper = $this->getHelper('question');
         $passwordQuestion = new Question('<question>Entrez le nouveau mot de passe root :</question> ');
 
@@ -48,20 +50,23 @@ class RootManageCommand extends Command
             ->setHidden(true)
             ->setHiddenFallback(false)
             ->setValidator(function (?string $value): string {
-                if (preg_match("/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{16,})/", $value)) {
+                if (null !== $value
+                    && 1 === preg_match('/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])(?=.{16,})/', $value)
+                ) {
                     return $value;
                 }
 
-                throw new \Exception('Le mot de passe ne répond pas aux exigences de sécurité.');
+                throw new \RuntimeException('Le mot de passe ne répond pas aux exigences de sécurité.');
             })
             ->setMaxAttempts(null)
         ;
 
         // Enregistrer la réponse
+        /** @var string $password */
         $password = $helper->ask($input, $output, $passwordQuestion);
 
         $root = $this->userRepository->findOneBy(['username' => 'root']);
-        if (!$root) { // Créer l'utilisateur root car il n'existe pas
+        if (null === $root) { // Créer l'utilisateur root car il n'existe pas
             $output->writeln("<comment>Le compte root n'existe pas. Il va être créé.</comment>");
             $root = new User();
             $root
@@ -69,7 +74,7 @@ class RootManageCommand extends Command
                 ->setPassword($this->passwordHasher->hashPassword($root, $password))
                 ->setRoles([
                     'ROLE_USER',
-                    'ROLE_ADMIN'
+                    'ROLE_ADMIN',
                 ])
             ;
             $this->em->persist($root);
