355f06e94cb8cd947df3096f1850460bd32c131b
Aurélien OUDART <aoudart@hexanet.fr>
2024-11-06 13:37:15 +0100

    Add custom css

--- importmap.php
+++ importmap.php
@@ -16,6 +16,10 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'logo' => [
+        'path' => './assets/logo.js',
+        'entrypoint' => true,
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
