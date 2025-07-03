7499c7fe1ce3ebbe8f7745c4f45aee9a0a150560
Koretech <oudart.aurelien@live.fr>
2025-07-03 13:13:33 +0200

    Update dependencies

--- importmap.php
+++ importmap.php
@@ -20,14 +20,14 @@ return [
         'path' => './assets/logo.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
     '@hotwired/turbo' => [
-        'version' => '7.3.0',
+        'version' => '8.0.13',
     ],
     'jquery' => [
         'version' => '3.7.1',
